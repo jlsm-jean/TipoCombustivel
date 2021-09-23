a4f5fcb0e9fcc600d8373f539ea05c9636588ae7
Jean Leonardo Soares de Moraes <jean.jlsm2@gmail.com>
2021-09-22 21:31:46 -0300

    Add project files.

---
+++ Tipo_Combustivel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tipo_Combustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tipo = 0;
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            while(tipo != 4)
+            {
+                tipo = int.Parse(Console.ReadLine());
+
+                if (tipo == 1)
+                    alcool += 1;
+                else if (tipo == 2)
+                    gasolina += 1;
+                else if (tipo == 3)
+                    diesel += 1;
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
